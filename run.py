dfa9390a57fbfaba0c050630149a34f91b012bd4
Vijay Shinde <98464217+VijayShinde1996@users.noreply.github.com>
2025-11-09 21:06:46 +0530

    Add files via upload
    Added new branch for developer specific work management

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ app = create_app()
 
 if __name__ == '__main__':
     from app.socket import socketio
+    print(f"\nAccess URLs:")
+    print(f"Local: http://localhost:{Config.PORT}")
+    print(f"LAN:   http://192.168.1.12:{Config.PORT}")
+    print("\nPress CTRL+C to quit\n")
     socketio.run(app, 
                 host=Config.HOST,
                 port=Config.PORT,
